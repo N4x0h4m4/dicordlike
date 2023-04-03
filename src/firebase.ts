413fac541ff891636b576453324a4513204f1ab5
N4x0h4m4 <n4x0leaf4m@gmail.com>
2023-04-03 21:05:56 +0900

    change

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRV5jJRu7oQoQ9hNcmvcVdT6IzvvD7BF4",
-  authDomain: "discord-clone-83b08.firebaseapp.com",
-  projectId: "discord-clone-83b08",
-  storageBucket: "discord-clone-83b08.appspot.com",
-  messagingSenderId: "393399833089",
-  appId: "1:393399833089:web:f60b51803644790a16e600",
+  //apiKey: "AIzaSyBRV5jJRu7oQoQ9hNcmvcVdT6IzvvD7BF4",
+  apiKey: "AIzaSyA5U8dSM5JPZiPDuPDE5H3ANIAlkI38AUw",
+  authDomain: "discordlike-97319.firebaseapp.com",
+  //projectId: "discord-clone-83b08",
+  projectId: "discordlike-97319",
+  storageBucket: "discordlike-97319.appspot.com",
+  //messagingSenderId: "393399833089",
+  messagingSenderId: "1072324506093",
+  //appId: "1:393399833089:web:f60b51803644790a16e600",
+  appId: "1:1072324506093:web:e0ef7e11dd1de56bb3f7e4",
 };
 
 const app = initializeApp(firebaseConfig);
